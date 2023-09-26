aebd7a244a2a8167b9d91adff86017b1f881ab28
Luka van der Plas <lukavdplas@gmail.com>
2023-09-26 11:06:07 +0200

    code quality

--- tests/test_validate.py
+++ tests/test_validate.py
@@ -2,7 +2,7 @@ from rdflib import Graph
 from pathlib import Path
 
 ONTOLOGY_FILE = Path(__file__).parent.parent / 'ontology.ttl'
-EXAMPLE_FILE =  Path(__file__).parent.parent / 'documentation/example.ttl'
+EXAMPLE_FILE =  Path(__file__).parent.parent / 'documentation' / 'example.ttl'
 
 def test_valid_turtle():
     g = Graph()
